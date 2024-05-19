758b985f7bce75fc9f4c80f94c76616e8a8645df
Karol Olbryś  <karlson2002@gmail.com>
2024-05-19 18:52:13 +0200

    Added constructors and methods

--- Project/Student.java
+++ Project/Student.java
@@ -1,3 +1,82 @@
+import java.util.Date;
+
 public class Student {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private Date birthDate;
+    private static int count;
+    private String indexNumber;
+    
+    public Student(String studentName, String studentSurname, String studentEmail, String studentAddress, String studentPhoneNumber, Date studentBirthDate) {
+        this.name = studentName;
+        this.surname = studentSurname;
+        this.email = studentEmail;
+        this.address = studentAddress;
+        this.phoneNumber = studentPhoneNumber;
+        this.birthDate = studentBirthDate;
+        count++;
+        this.indexNumber = "s" + count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+
+    public String getIndexNumber() {
+        return indexNumber;
+    }
+
+
+    
+
     
 }
---
+++ Project/StudyProgramme.java
@@ -0,0 +1,16 @@
+public class StudyProgramme {
+    private String programmeName;
+    private String description;
+    private int numberOfSemesters;
+    private int maxItn;
+        
+    public StudyProgramme(String programmeName, String description, int numberOfSemesters, int maxItn) {
+        this.programmeName = programmeName;
+        this.description = description;
+        this.numberOfSemesters = numberOfSemesters;
+        this.maxItn = maxItn;
+    }
+
+    
+
+}
